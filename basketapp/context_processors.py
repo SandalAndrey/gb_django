ecda4677336f771c73d440727d29853fbaebc25e
Sandal Andrey <andrey@sandal.org.ru>
2018-10-29 13:12:11 +0300

    CBV

--- basketapp/context_processors.py
+++ basketapp/context_processors.py
@@ -5,9 +5,9 @@ from django.db.models import Sum
 
 def include_basket(request):
     basket=[]
+    total_price = 0
     if request.user.is_authenticated:
         basket = Basket.objects.filter(user=request.user)
-        total_price = 0
         for item in basket:
             total_price+=item.product.price*item.quantity
 
